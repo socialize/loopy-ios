ffc4f4781ab7634085836246808f558a078cb7ed
djedeikin <djedeikin@sharethis.com>
2013-09-11 13:43:07 -0700

    initial commit

---
+++ TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Loopy
+//
+//  Created by David Jedeikin on 9/10/13.
+//  Copyright (c) 2013 ShareThis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SZAppDelegate class]));
+    }
+}
