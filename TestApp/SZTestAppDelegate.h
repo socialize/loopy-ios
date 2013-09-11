e62f80d0e97fdbfb0afc66b7d356f48b062beb3f
djedeikin <djedeikin@sharethis.com>
2013-09-11 15:52:49 -0700

    Added GHUnit framework

---
+++ TestApp/SZTestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SZAppDelegate.h
+//  Loopy
+//
+//  Created by David Jedeikin on 9/10/13.
+//  Copyright (c) 2013 ShareThis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SZTestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UnitTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UnitTests
+//
+//  Created by David Jedeikin on 9/11/13.
+//  Copyright (c) 2013 ShareThis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SZTestAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
